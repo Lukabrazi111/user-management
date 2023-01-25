1617186ae64eeed745e8bdfb039a66b59005a4c8
Lukabrazi111 <lukabrazi@redberry.ge>
2023-01-25 23:26:24 +0400

    Revert "chore: rename folder"
    This reverts commit 1ce06e100767b52e096b798b7e62b73580595dc3.

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -12,7 +12,7 @@ return new class extends Migration {
      */
     public function up()
     {
-        Schema::create('todo', function (Blueprint $table) {
+        Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('first_name');
             $table->string('last_name');
@@ -33,6 +33,6 @@ return new class extends Migration {
      */
     public function down()
     {
-        Schema::dropIfExists('todo');
+        Schema::dropIfExists('users');
     }
 };
